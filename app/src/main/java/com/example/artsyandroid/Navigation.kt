dfc8a2a619c41bf521cf3963d8d667a88a3db06a
Karthik Venugopal <kvenugop@usc.edu>
2025-04-16 18:28:56 -0700

    Removed unused import

--- app/src/main/java/com/example/artsyandroid/Navigation.kt
+++ app/src/main/java/com/example/artsyandroid/Navigation.kt
@@ -3,7 +3,6 @@
 package com.example.artsyandroid
 
 import android.content.Intent
-import android.net.Uri
 import android.util.Log
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
